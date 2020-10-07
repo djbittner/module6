9e7183606735191a0eba5843309b120f0be44b6e
djbittner <djbittner@dmacc.edu>
2020-10-06 22:04:26 -0500

    final string_functions.py pass

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -9,6 +9,10 @@ Due Date:  10/6/2020
 def multiply_string(message, n):
     result = message * n
     print(message * n)
-    pass
+    return result
+
+
+if __name__ == '__main__':
+    multiply_string("Derek", 3)
 
 
--- test_functions/test_string_functions.py
+++ test_functions/test_string_functions.py
@@ -4,7 +4,7 @@ from more_functions import string_functions
 
 class MyTestCase(unittest.TestCase):
     def test_multiply_string(self):
-        self.assertEqual("DerekDerekDerek", string_functions.multiply_string("Derek", 5))
+        self.assertEqual("DerekDerekDerek", string_functions.multiply_string("Derek", 3))
 
 
 if __name__ == '__main__':
