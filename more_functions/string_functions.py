709c54eb33379cb7b71bcb967618e750c497c451
djbittner <djbittner@dmacc.edu>
2020-10-06 22:01:49 -0500

    initial commit fail with pass

---
+++ more_functions/string_functions.py
@@ -0,0 +1,14 @@
+"""
+
+Author:  Derek Bittner
+Due Date:  10/6/2020
+
+"""
+
+
+def multiply_string(message, n):
+    result = message * n
+    print(message * n)
+    pass
+
+
---
+++ test_functions/test_string_functions.py
@@ -0,0 +1,11 @@
+import unittest
+from more_functions import string_functions
+
+
+class MyTestCase(unittest.TestCase):
+    def test_multiply_string(self):
+        self.assertEqual("DerekDerekDerek", string_functions.multiply_string("Derek", 5))
+
+
+if __name__ == '__main__':
+    unittest.main()
